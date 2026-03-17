63287a006fecfb42ee1fd79bce49363d47586f7d
iranna <irannajangannavar00@gmail.com>
2026-03-17 12:28:34 +0530

    Star commit

---
+++ Starpattern6.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Starpattern6 {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(i==1 || i==n ||i==j || i+j==n+1){
+                    System.out.print("x ");
+                }
+                else{
+                    System.out.print("  "); //2 white or  System.out.print("o ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+}
